5eef1d7cbd9c08f617ac28c891249cd1223f0cbe
mnsekh111 <mnsekh111@gmail.com>
2016-03-19 01:37:54 -0400

    Added UI for PC2 client

--- Arduino/ArduinoSerialComm/src/iot/ArdSerial.java
+++ Arduino/ArduinoSerialComm/src/iot/ArdSerial.java
@@ -11,62 +11,70 @@ import com.fazecast.jSerialComm.*;
  *
  * @author root
  */
+class PacketListener implements Runnable, SerialPortPacketListener {
+
+    @Override
+    public int getListeningEvents() {
+        return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+    }
+
+    @Override
+    public int getPacketSize() {
+        return 100;
+    }
+
+    @Override
+    public void serialEvent(SerialPortEvent event) {
+        byte[] newData = event.getReceivedData();
+        System.out.println("Received data of size: " + newData.length);
+        for (int i = 0; i < newData.length; ++i) {
+            System.out.print((char) newData[i]);
+        }
+        System.out.println("\n");
+    }
+
+    @Override
+    public void run() {
+
+    }
+}
+
 public class ArdSerial {
 
-    private static final class PacketListener implements SerialPortPacketListener {
+    private static ArdSerial instance = null;
+    private SerialPort comPort;
+    private PacketListener listener = null;
 
-        @Override
-        public int getListeningEvents() {
-            return SerialPort.LISTENING_EVENT_DATA_RECEIVED;
+    private ArdSerial() {
+        comPort = SerialPort.getCommPorts()[0];
+        listener = new PacketListener();
+
+    }
+
+    public static ArdSerial getInstance(){
+        if(instance == null){
+            return new ArdSerial();
         }
+        return instance;
+    }
+    
+    public void start() {
 
-        @Override
-        public int getPacketSize() {
-            return 100;
-        }
-
-        @Override
-        public void serialEvent(SerialPortEvent event) {
-            byte[] newData = event.getReceivedData();
-            System.out.println("Received data of size: " + newData.length);
-            for (int i = 0; i < newData.length; ++i) {
-                System.out.print((char) newData[i]);
-            }
-            System.out.println("\n");
+        if (!comPort.isOpen()) {
+            comPort.openPort();
+            comPort.addDataListener(listener);
         }
     }
 
-    public static void main(String[] args) {
-//        SerialPort comPort = SerialPort.getCommPorts()[0];
-//        comPort.openPort();
-//        comPort.addDataListener(new SerialPortDataListener() {
-//            @Override
-//            public int getListeningEvents() {
-//                return SerialPort.LISTENING_EVENT_DATA_AVAILABLE;
-//            }
-//
-//            @Override
-//            public void serialEvent(SerialPortEvent event) {
-//                if (event.getEventType() != SerialPort.LISTENING_EVENT_DATA_AVAILABLE) {
-//                    return;
-//                }
-//                byte[] newData = new byte[comPort.bytesAvailable()];
-//                int numRead = comPort.readBytes(newData, newData.length);
-//                System.out.println("Read " + numRead + " bytes.");
-//
-//            }
-//        });
-
-        SerialPort comPort = SerialPort.getCommPorts()[0];
-        comPort.openPort();
-        PacketListener listener = new PacketListener();
-        comPort.addDataListener(listener);
-        try {
-            Thread.sleep(5000);
-        } catch (Exception e) {
-            e.printStackTrace();
+    public void stop() {
+        if (comPort.isOpen()) {
+            comPort.removeDataListener();
+            comPort.closePort();
         }
-        comPort.removeDataListener();
-        comPort.closePort();
+        deinit();
+    }
+    
+    private void deinit(){
+        instance = null;
     }
 }
