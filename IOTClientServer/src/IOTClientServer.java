610569eebc4fd09df70596716e371495aabc0d96
mnsekh111 <mnsekh111@gmail.com>
2016-03-20 18:53:17 -0400

    Added multiple thread handler for PC1 and PC2 on Rasp pi server

--- IOTClientServer/src/IOTClientServer.java
+++ IOTClientServer/src/IOTClientServer.java
@@ -11,43 +11,17 @@ import java.io.PrintWriter;
 import java.net.ServerSocket;
 import java.net.Socket;
 
-public class IOTClientServer {
+class PC1Handler extends Thread {
+	private Socket clientSocket = null;
 
-	// static {
-	// try {
-	// System.loadLibrary("mraajava");
-	// } catch (UnsatisfiedLinkError e) {
-	// System.err
-	// .println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n"
-	// + e);
-	// System.exit(1);
-	// }
-	// }
+	public PC1Handler(Socket soc) {
+		this.clientSocket = soc;
+	}
 
-	public static void main(String[] args) {
-		int portNum = 9991;
-
-		ServerSocket serverSocket = null;
-		Socket clientSocket = null;
-
+	@Override
+	public void run() {
 		try {
-			serverSocket = new ServerSocket(portNum);
-			System.out.println("Listening on port " + portNum);
-		} catch (IOException ie) {
-			System.err.println("Error listening at port " + portNum + "\n"
-					+ ie.getLocalizedMessage());
-			System.exit(1);
-		}
-
-		while (true) {
-			try {
-				clientSocket = serverSocket.accept();
-			} catch (IOException e) {
-				System.err.println("Accept failed.");
-				System.exit(1);
-			}
-
-			try {
+			if (clientSocket != null) {
 				File file = new File("samplefile");
 				FileOutputStream fout = new FileOutputStream(file);
 				BufferedInputStream in = new BufferedInputStream(
@@ -62,16 +36,101 @@ public class IOTClientServer {
 				in.close();
 				fout.close();
 				clientSocket.close();
-
-			} catch (FileNotFoundException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
-			} catch (IOException e) {
-				// TODO Auto-generated catch block
-				e.printStackTrace();
 			}
-
+		} catch (FileNotFoundException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
+
+	}
+}
+
+class PC2Handler extends Thread {
+	private Socket clientSocket = null;
+
+	public PC2Handler(Socket soc) {
+		this.clientSocket = soc;
 	}
 
+	@Override
+	public void run() {
+		try {
+			if (clientSocket != null) {
+
+				BufferedInputStream in = new BufferedInputStream(
+						clientSocket.getInputStream());
+
+			}
+
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+
+	}
+}
+
+public class IOTClientServer {
+
+	// uncomment the following portion for running this code in Intel Edison
+	// "from Eclipse"
+	// static {
+	// try {
+	// System.loadLibrary("mraajava");
+	// } catch (UnsatisfiedLinkError e) {
+	// System.err
+	// .println("Native code library failed to load. See the chapter on Dynamic Linking Problems in the SWIG Java documentation for help.\n"
+	// + e);
+	// System.exit(1);
+	// }
+	// }
+
+	public static void main(String[] args) {
+		int portNum = 9991;
+		boolean runforever = true;
+
+		ServerSocket serverSocket = null;
+		Socket clientSocket = null;
+
+		try {
+			serverSocket = new ServerSocket(portNum);
+			System.out.println("Listening on port " + portNum);
+		} catch (IOException ie) {
+			System.err.println("Error listening at port " + portNum + "\n"
+					+ ie.getLocalizedMessage());
+			System.exit(1);
+		}
+
+		while (runforever) {
+			try {
+				clientSocket = serverSocket.accept();
+			} catch (IOException e) {
+				System.err.println("Accept failed.");
+				System.exit(1);
+			}
+
+			String source = clientSocket.getInetAddress()
+					.getCanonicalHostName();
+			// System.out.println(source);
+			if (source.contentEquals("pc1-client")) {
+				PC1Handler handler = new PC1Handler(clientSocket);
+				handler.start();
+
+			} else if (source.contentEquals("pc2-client")) {
+				PC2Handler handler = new PC2Handler(clientSocket);
+				handler.start();
+
+			}
+		}
+
+		try {
+			serverSocket.close();
+		} catch (IOException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+		}
+	}
 }
