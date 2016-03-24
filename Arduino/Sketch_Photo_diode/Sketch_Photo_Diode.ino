b1a70f3a0acbeb13db884f3a50dffff4a7ddaefb
Michael Xu <mxu@ncsu.edu>
2016-03-24 16:35:05 -0400

    fixed calibration.

--- Arduino/Sketch_Photo_diode/Sketch_Photo_Diode.ino
+++ Arduino/Sketch_Photo_diode/Sketch_Photo_Diode.ino
@@ -20,23 +20,25 @@ int lightPin = 0;  //define a pin for Photo resistor
 int ledPin = 11;     //define a pin for LED
 int normalLight = 0;  //calibration for normal light value; leave at 0 for calibration to determine
 int highLight = 0;  //calibration for normal light value; leave at 0 for calibration to determine
-float normal_tolerance = 1.30; //accept values within 130% of normal/low
-float high_tolerance =  0.70; //accept values within 70% of high for high.
-
+float midway =0;
+int calibration_time=10;
 
 void setup()
 {
     Serial.begin(9600);  //Begin serial communcation
     pinMode( ledPin, OUTPUT );
     
+    Serial.print("calibration starting. you have ");
+    Serial.print(calibration_time);
+    Serial.println(" seconds to show the photodiode an example of off and on."); //
 
 
-    if (normalLight==0){
+    if (normalLight==0 or highLight==0){
       normalLight=9000;
       unsigned long starttime = millis();
       unsigned long endtime = starttime;
       int analogReading=0;
-      while ((endtime - starttime) <=20*1000) // do this loop 20 Seconds
+      while ((endtime - starttime) <=calibration_time*1000) // do this loop 20 Seconds
       {
         analogReading=analogRead(lightPin);
         if (analogReading<normalLight){ //recording the lowest value of light
@@ -48,28 +50,28 @@ void setup()
       endtime = millis();
       }
     }
-    
+
+    midway=(highLight-normalLight)/2 + normalLight;
     Serial.print("debug: normalLight set to:"); //
     Serial.println(normalLight); //
     Serial.print("debug: highLight set to:"); 
     Serial.println(highLight); 
-    Serial.print("debug: normalLight tolerance set to:"); //
-    Serial.println(normalLight*normal_tolerance); //
-    Serial.print("debug: highLight tolerance set to:"); //
-    Serial.println(normalLight*high_tolerance); //
+    Serial.print("debug: midway set to:"); //
+    Serial.println(midway); //
 
 }
 
 void loop()
 {
     int analogReading=analogRead(lightPin);
-    if (analogReading<normalLight*normal_tolerance)
+    if (analogReading<midway)
       Serial.print("0"); //Write the value of the photodiode to the serial monitor.
-    else if (analogReading>highLight*high_tolerance)
+    else if (analogReading>midway)
       Serial.print("1"); //Write the value of the photodiode to the serial monitor.
 //    else
 //      Serial.print("???:"); //Write the value of the photodiode to the serial monitor.
-    //Serial.println(analogReading);          
+//    Serial.print(":");          
+//    Serial.println(analogReading);          
     
-   delay(10); //short delay for faster response to light.
+   delay(100); //short delay for faster response to light.
 }
