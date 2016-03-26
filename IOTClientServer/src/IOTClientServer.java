1d08c57988d19668ed22730f6ee8f4253a656ebf
dhaval0603 <dhaval0603@gmail.com>
2016-03-26 19:52:54 -0400

    IOT CLient Server changes

--- IOTClientServer/src/IOTClientServer.java
+++ IOTClientServer/src/IOTClientServer.java
@@ -120,6 +120,11 @@ public class IOTClientServer {
 								
 								break;
 							} else {
+								try {
+									Thread.sleep(500);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
 								continue;
 							}
 						}
@@ -189,6 +194,11 @@ public class IOTClientServer {
 								
 								break;
 							} else {
+								try {
+									Thread.sleep(500);
+								} catch (InterruptedException e) {
+									e.printStackTrace();
+								}
 								continue;
 							}
 
