7569291b0800222cb6b35b7042495ffd85a32caa
mnsekh111 <mnsekh111@gmail.com>
2016-04-03 19:38:29 -0400

    added serial code
    put this inside another thread

---
+++ Assign3/simulator/serial.py
@@ -0,0 +1,13 @@
+#pip install pySerial
+
+import serial
+import time
+
+ser = serial.Serial('COM3', 9600, timeout=0)
+ 
+while 1:
+    try:
+        print ser.readline()
+        time.sleep(1)
+    except ser.SerialTimeoutException:
+        print('Data could not be read')
