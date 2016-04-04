9e5f42a530c1ffa237448cc0c36ffe389ccd117d
dhaval0603 <dhaval0603@gmail.com>
2016-04-04 19:24:13 -0400

    More structure to Laptop2 and Raspberry code

--- Assign3/Laptop2/laptop2.py
+++ Assign3/Laptop2/laptop2.py
@@ -1,8 +1,27 @@
 import paho.mqtt.client as paho
 import time
+import serial
 
 def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
+
+def on_message(client, userdata, msg):
+    print("[" + time.ctime() + "] Topic = " + msg.topic + ", QoS = " + str(msg.qos)+", Payload = "+str(msg.payload))
+
+
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    client.publish("status/Arduino", payload="Online", qos=2, retain=True)
+    client.subscribe([("lightSensor",2),("threshold",2)])
+
+def on_disconnect(client, userdata, rc):
+    client.publish("status/Arduino", payload="Offline", qos=2, retain=True)
+
+
+#Get Serial connection
+ser = serial.Serial('COM3', 9600, timeout=0)
+print ser.readline()
+
 
 #Create client
 client = paho.Client("Laptop2")
@@ -12,11 +31,13 @@ client.will_set("status/Arduino", payload="Offline", qos=2, retain=True)
 
 #Set callbacks
 client.on_publish = on_publish
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
 
 #Connect to Broker
 client.connect("192.168.0.180", 1883)
 
-#Publish an "Online" message
-client.publish("status/Arduino", payload="Online", qos=2, retain=True)
-
 client.loop_forever()
+
+
--- Assign3/Raspberry/raspberry.py
+++ Assign3/Raspberry/raspberry.py
@@ -4,6 +4,18 @@ import time
 def on_publish(client, userdata, mid):
     print("mid: "+str(mid))
 
+def on_message(client, userdata, msg):
+    print("[" + time.ctime() + "] Topic = " + msg.topic + ", QoS = " + str(msg.qos)+", Payload = "+str(msg.payload))
+
+def on_connect(client, userdata, rc):
+    print("Connected with result code "+str(rc))
+    client.publish("status/Raspi", payload="Online", qos=2, retain=True)
+    client.subscribe([("lightSensor",2),("threshold",2)])
+
+def on_disconnect(client, userdata, rc):
+    client.publish("status/Raspi", payload="Offline", qos=2, retain=True)
+
+
 #Create client
 client = paho.Client("RaspberryPi")
 
@@ -12,11 +24,11 @@ client.will_set("status/Raspi", payload="Offline", qos=2, retain=True)
 
 #Set callbacks
 client.on_publish = on_publish
+client.on_message = on_message
+client.on_connect = on_connect
+client.on_disconnect = on_disconnect
 
 #Connect to Broker
 client.connect("192.168.0.180", 1883)
-
-#Publish an "Online" message
-client.publish("status/Raspi", payload="Online", qos=2, retain=True)
 
 client.loop_forever()
