d6c9cdd34ef860642b23b71eac592c6e8aa10f53
mnsekh111 <mnsekh111@gmail.com>
2016-04-18 09:24:49 -0400

    Final add +

--- Assign4/Submission/train2.py
+++ Assign4/Submission/train2.py
@@ -4,6 +4,9 @@ import os
 
 import csv
 
+min_val = [100000.0] * 5
+max_val = [-100000.0] * 5
+
 
 def csv_to_libsvm(file_path, header, class_ind, num_cols):
     data_v = []
@@ -52,34 +55,56 @@ def csv_to_libsvm(file_path, header, class_ind, num_cols):
                     class_v.append(0.0)
 
         fin.close()
+
+    for indi in range(0, len(data_v[0])):
+        for indj in range(0, len(data_v)):
+            if min_val[indi] > data_v[indj][indi + 1]:
+                min_val[indi] = data_v[indj][indi + 1]
+            if max_val[indi] < data_v[indj][indi + 1]:
+                max_val[indi] = data_v[indj][indi + 1]
+
+    for indi in range(0, len(data_v[0])):
+        for indj in range(0, len(data_v)):
+            data_v[indj][indi + 1] = 1.0 / (max_val[indi] - min_val[indi]) * (data_v[indj][indi + 1] - min_val[indi])
+
+    # print min_val, max_val
+
+    if len(class_v) != len(data_v):
+        class_v = [0] * len(data_v)
     return class_v, data_v
 
 
 # Class map to represent string as integer/float which is required by libsvm format
 class_map = {"absent": 0.0, "present": 1.0}
 rev_class_map = {0.0: "absent", 1.0: "present"}
-data_vector = []
-class_vector = []
 
-# Convert from csv to libsvm format
-class_vector, data_vector = csv_to_libsvm(
-    ["Occupancy Dataset/datatest2.csv"], True, 5,
-    6)
+if os.path.exists("./svm.model") == False:
 
-# Debug statement
-# print data_vector
+    data_vector = []
+    class_vector = []
 
-# Creating a new parameter
-param = svm_parameter('-t 2 -g .00001 -b 1 -c 512')
-# Creating a problem set from data and class vectors
-problem = svm_problem(class_vector, data_vector)
-# Generating a model only if no model file is present
+    # Convert from csv to libsvm format
+    class_vector, data_vector = csv_to_libsvm(
+        ["Occupancy Dataset/datatest1.csv", "Occupancy Dataset/datatest2.csv", "Occupancy Dataset/datatest3.csv"], True,
+        5,
+        6)
 
-if os.path.exists("./svm.model"):
-    model = svm_load_model("./svm.model")
-else:
+    # Debug statement
+    # print data_vector
+
+    # Creating a new parameter
+
+    param = svm_parameter('-t 2 -h 0 -c 8 -g 8')
+
+    # Creating a problem set from data and class vectors
+    problem = svm_problem(class_vector, data_vector)
+    # Generating a model only if no model file is present
+
     model = svm_train(problem, param)
-    svm_save_model("./svm.model", model)
+    svm_save_model("svm.model", model)
+
+else:
+    model = svm_load_model("svm.model")
 
 # Debug statement
 # print model
@@ -91,10 +116,9 @@ test_class_vector = []
 # Here test_class_vector will be empty
 # If no class is specified the test.csv, then set the second parameter to -1
 test_class_vector, test_data_vector = csv_to_libsvm(
-    ["test.csv"], False, -1, 5)
+    ["test.csv"], True, -1, 5)
 
-# test_class_vector = [0] * len(test_data_vector);
-p_label, p_acc, p_val = svm_predict(test_class_vector, test_data_vector, model, '-b 1')
+p_label, p_acc, p_val = svm_predict(test_class_vector, test_data_vector, model)
 
 # Debug statement
 # print p_label, p_acc, p_val
@@ -103,6 +127,6 @@ test_class_vector = p_label
 target = open("result.csv", 'w')
 # Writing to the output file
 for i in range(0, len(test_class_vector)):
-    target.write(rev_class_map[test_class_vector[i]] + "\n")
+    target.write(rev_class_map[test_class_vector[i]] + ",")
 
 target.close()
